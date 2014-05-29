a72c94d85e82c97a42755dc70eee8d87eedcda28
renehernandez <renehr9102@gmail.com>
2014-05-28 17:51:51 -0700

    Implenting BFS of vertices and creating first test
    Methods:
    - IEnumerable<T> Vertices<T>(...) in class BFS
    
    Test:
    - FromEndOfPathGraph

---
+++ Code/SmartNet.UnitTest/BFSTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SmartNet.Algorithms.Traversal;
+
+namespace SmartNet.UnitTest
+{
+    [TestFixture]
+    public class BFSTest
+    {
+
+        private Graph<string> graph;
+
+        private List<string> stringData;
+
+        [SetUp]
+        public void Init()
+        {
+            graph = new Graph<string>();
+            stringData = new List<string> {"hello", "black", "watchout", "game of thrones", "maroon 5"};
+        }
+
+        [Test]
+        public void FromEndOfPathGraph()
+        {
+            graph.AddPath(stringData);
+
+            var expectedVertices = new string[] {"watchout", "black", "game of thrones", "hello", "maroon 5"};
+
+            CheckValues(expectedVertices, BFS.Vertices(graph, "watchout").ToArray());
+
+        }
+
+        # region Private Methods
+
+        private void CheckValues<T>(T[] array1, T[] array2)
+        {
+            Assert.AreEqual(array1.Length, array2.Length);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Assert.AreEqual(array1[i], array2[i]);
+            }
+        }
+
+        # endregion
+
+    }
+}
--- Code/SmartNet/Algorithms/Traversal/BFS.cs
+++ Code/SmartNet/Algorithms/Traversal/BFS.cs
@@ -9,5 +9,26 @@ namespace SmartNet.Algorithms.Traversal
     public static class BFS
     {
 
+        public static IEnumerable<T> Vertices<T>(Graph<T> graph, T vertex) where T : IEquatable<T>
+        {
+            var mark = new HashSet<T>() {vertex};
+            var queue = new Queue<T>();
+
+            T current;
+            queue.Enqueue(vertex);
+
+            while (queue.Count != 0)
+            {
+                current = queue.Dequeue();
+                yield return current;
+
+                foreach (var neighbor in graph.NeighborsIterator(current).Where(neighbor => !mark.Contains(neighbor)))
+                {
+                    mark.Add(neighbor);
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }  
+
     }
 }
