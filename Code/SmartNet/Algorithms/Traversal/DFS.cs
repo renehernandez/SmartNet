9158c469b08fe9cdcbf4d8975682e9d4a4ee6919
renehernandez <renehr9102@gmail.com>
2014-05-30 12:52:34 -0700

    Refactoring method for dfs.
    Now for:
    Vertices(Graph<T> graph) -> returns a dfs for entire graph (a forest if graph is not connected)
    Vertices(Graph<T> graph, T vertex) -> returns a dfs for all the vertices reachable from source vertex

--- Code/SmartNet/Algorithms/Traversal/DFS.cs
+++ Code/SmartNet/Algorithms/Traversal/DFS.cs
@@ -11,10 +11,15 @@ namespace SmartNet.Algorithms.Traversal
 
         public static IEnumerable<T> Vertices<T>(Graph<T> graph) where T : IEquatable<T>
         {
-            return Vertices(graph, graph.VerticesIterator.First());
+            return Vertices(graph, graph.VerticesIterator.First(), true);
         }
 
         public static IEnumerable<T> Vertices<T>(Graph<T> graph, T vertex) where T : IEquatable<T>
+        {
+            return Vertices(graph, vertex, false);
+        }
+
+        private static IEnumerable<T> Vertices<T>(Graph<T> graph, T vertex, bool complete) where T : IEquatable<T>
         {
             var mark = new HashSet<T> {vertex};
 
@@ -22,26 +27,24 @@ namespace SmartNet.Algorithms.Traversal
             foreach (var w in VerticesVisit(graph, vertex, mark))
                 yield return w;
 
-            foreach (var w in graph.VerticesIterator)
+            if (!complete) 
+                yield break;
+            
+            foreach (var w in graph.VerticesIterator.Where(w => !mark.Contains(w)))
             {
-                if (mark.Contains(w))
-                    continue;
-
                 mark.Add(w);
 
                 foreach (var v in VerticesVisit(graph, w, mark))
                     yield return v;
             }
-
         }
+
+
 
         private static IEnumerable<T> VerticesVisit<T>(Graph<T> graph, T vertex, HashSet<T> mark) where T : IEquatable<T>
         {
-            foreach (var w in graph.NeighborsIterator(vertex))
+            foreach (var w in graph.NeighborsIterator(vertex).Where(w => !mark.Contains(w)))
             {
-                if (mark.Contains(w))
-                    continue;
-
                 mark.Add(w);
                 yield return w;
 
@@ -52,21 +55,26 @@ namespace SmartNet.Algorithms.Traversal
 
         public static IEnumerable<Edge<T>> Edges<T>(Graph<T> graph) where T: IEquatable<T>
         {
-            return Edges(graph, graph.VerticesIterator.First());
+            return Edges(graph, graph.VerticesIterator.First(), true);
         }
 
         public static IEnumerable<Edge<T>> Edges<T>(Graph<T> graph, T vertex) where T: IEquatable<T>
+        {
+            return Edges(graph, vertex, false);
+        } 
+
+        private static IEnumerable<Edge<T>> Edges<T>(Graph<T> graph, T vertex, bool complete) where T: IEquatable<T>
         {
             var mark = new HashSet<T> {vertex};
 
             foreach (var edge in EdgesVisit(graph, vertex, mark))
                 yield return edge;
 
-            foreach(var w in graph.VerticesIterator)
-            {
-                if (mark.Contains(w))
-                    continue;
+            if (!complete)
+                yield break;
 
+            foreach (var w in graph.VerticesIterator.Where(w => !mark.Contains(w)))
+            {
                 mark.Add(w);
                 foreach (var edge in EdgesVisit(graph, w, mark))
                     yield return edge;
@@ -77,11 +85,8 @@ namespace SmartNet.Algorithms.Traversal
         private static IEnumerable<Edge<T>> EdgesVisit<T>(Graph<T> graph, T v, HashSet<T> mark) 
             where T: IEquatable<T>
         {
-            foreach (var keyValue in graph[v])
+            foreach (var keyValue in graph[v].Where(keyValue => !mark.Contains(keyValue.Key)))
             {
-                if (mark.Contains(keyValue.Key))
-                    continue;
-
                 mark.Add(keyValue.Key);
                 yield return keyValue.Value;
 
@@ -89,6 +94,5 @@ namespace SmartNet.Algorithms.Traversal
                     yield return edge;
             }
         }
-
     }
 }
