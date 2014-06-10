2dd02be0927fdfd3deabe0b220ae868e5eb58979
renehernandez <renehr9102@gmail.com>
2014-06-10 00:32:46 -0700

    Adding simple classes for handling common cases of graph interactions
    Classes:
    - SGraph<TVertex> for creating a Graph<TVertex, SEdge<TVertex>, Data>
    - SDiGraph<TVertex> for creating a DiGraph<TVertex, SDiEdge<TVertex>, Data>
    
    These classes use the following auxiliary classes:
    - SEdge<TVertex> for creating an Edge<TVertex, Data>
    - SDiEdge<TVertex> for creating an DiEdge<TVertex, Data>

---
+++ Code/SmartNet/SDiGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartNet
+{
+    public class SDiGraph<TVertex> : DiGraph<TVertex, SDiEdge<TVertex>, Data> 
+        where TVertex : IEquatable<TVertex>
+    {
+    }
+}
---
+++ Code/SmartNet/SGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartNet
+{
+    public class SGraph<TVertex> : Graph<TVertex, SEdge<TVertex>, Data> 
+        where TVertex : IEquatable<TVertex>
+    {
+    }
+}
