543311d0bd30554b869c800f41b842211aebdb37
renehernandez <renehr9102@gmail.com>
2014-06-13 00:50:38 -0700

    Updating Classic class for change in graph and related classes

--- Code/SmartNet/Models/Classic.cs
+++ Code/SmartNet/Models/Classic.cs
@@ -10,10 +10,11 @@ namespace SmartNet.Models
     public static class Classic
     {
 
-        public static TGraph EmptyGraph<TGraph, TEdge, TData>(int n)
-            where TGraph : IGraph<TGraph, int, TEdge, TData>, new()
-            where TEdge : IEdge<TEdge, int, TData>
-            where TData : IData, new()
+        public static TGraph EmptyGraph<TGraph, TEdge, TGraphData, TEdgeData>(int n)
+            where TGraph : IGraph<TGraph, int, TEdge, TGraphData, TEdgeData>, new()
+            where TEdge : IEdge<TEdge, int, TEdgeData>
+            where TEdgeData : IEdgeData, new() 
+            where TGraphData : IGraphData, new()
         {
             var graph = new TGraph();
             if (n > 0)
@@ -22,9 +23,9 @@ namespace SmartNet.Models
         }
 
         public static TGraph CompleteGraph<TGraph>(int n)
-            where TGraph : IGraph<TGraph, int, SEdge<int>, Data>, new()
+            where TGraph : IGraph<TGraph, int, SEdge<int>, GraphData, EdgeData>, new()
         {
-            var graph = EmptyGraph<TGraph, SEdge<int>, Data>(n);
+            var graph = EmptyGraph<TGraph, SEdge<int>, GraphData, EdgeData>(n);
 
             graph.AddEdges(Enumerable.Range(0, n).SelectMany(
                 i => Enumerable.Range(i + 1, n).Select(j => new SEdge<int>(i, j))
