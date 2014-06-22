08b3d5b7f1d0b1ad3cf0194233847b10aa56e621
renehernandez <renehr9102@gmail.com>
2014-06-22 13:27:33 -0700

    Adding house and houseX named graphs

--- Code/SmartNet/Models/Named.cs
+++ Code/SmartNet/Models/Named.cs
@@ -10,6 +10,58 @@ namespace SmartNet.Models
     public class Named
     {
 
+        # region House Graph
+
+        public static SGraph<int> HouseGraph()
+        {
+            return HouseGraph<SGraph<int>, SEdge<int>, GraphData, EdgeData>();
+        }
+
+        public static TGraph HouseGraph<TGraph, TEdge, TGraphData, TEdgeData>()
+            where TGraph : Graph<int, TEdge, TGraphData, TEdgeData>, IGraph<TGraph, int, TEdge, TGraphData, TEdgeData>,
+                new()
+            where TEdge : Edge<int, TEdgeData>, IEdge<TEdge, int, TEdgeData>
+            where TGraphData : IGraphData, new()
+            where TEdgeData : IEdgeData, new()
+        {
+
+            var graph = new TGraph();
+
+            graph.AddCycle(Enumerable.Range(0, 5));
+            graph.AddEdge(1, 4);
+            graph.Data.Name = "House Graph";
+
+            return graph;
+        }
+
+        public static SGraph<int> HouseXGraph()
+        {
+            return HouseGraph<SGraph<int>, SEdge<int>, GraphData, EdgeData>();
+        }
+
+        public static TGraph HouseXGraph<TGraph, TEdge, TGraphData, TEdgeData>()
+            where TGraph : Graph<int, TEdge, TGraphData, TEdgeData>, IGraph<TGraph, int, TEdge, TGraphData, TEdgeData>,
+                new()
+            where TEdge : Edge<int, TEdgeData>, IEdge<TEdge, int, TEdgeData>
+            where TGraphData : IGraphData, new()
+            where TEdgeData : IEdgeData, new()
+        {
+
+            var graph = new TGraph();
+
+            graph.AddCycle(Enumerable.Range(0, 5));
+            graph.AddEdge(1, 4);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(4, 2);
+            graph.Data.Name = "House X Graph";
+
+            return graph;
+        }
+
+        # endregion
+
+        # region Petersen Graph
+
         public static SGraph<int> PetersenGraph()
         {
             return PetersenGraph<SGraph<int>, SEdge<int>, GraphData, EdgeData>();
@@ -30,6 +82,8 @@ namespace SmartNet.Models
 
             return graph;
         }
+
+        # endregion
 
     }
 }
